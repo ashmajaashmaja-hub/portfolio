eec491bd2c9c1816ab15a62c25a9d0f48e383fba
ashmajaashmaja-hub <ashmajaashmaja@gmail.com>
2026-06-21 19:45:37 +0530

    Added calculator project

---
+++ calculator.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+def click(value):
+    current = entry.get()
+    entry.delete(0, END)
+    entry.insert(END, current + str(value))
+
+def clear():
+    entry.delete(0, END)
+
+def calculate():
+    try:
+        result = eval(entry.get())
+        entry.delete(0, END)
+        entry.insert(END, str(result))
+    except:
+        entry.delete(0, END)
+        entry.insert(END, "Error")
+
+root = Tk()
+root.title("Calculator")
+root.geometry("300x400")
+
+entry = Entry(root, font=("Arial", 20), justify="right",width=18,bg="white",fg="black")
+entry.grid(row=0, column=0, columnspan=4, padx=10, pady=10,ipady=10)
+
+buttons = [
+    ('7',1,0), ('8',1,1), ('9',1,2), ('/',1,3),
+    ('4',2,0), ('5',2,1), ('6',2,2), ('*',2,3),
+    ('1',3,0), ('2',3,1), ('3',3,2), ('-',3,3),
+    ('0',4,0), ('.',4,1), ('=',4,2), ('+',4,3)
+]
+
+for (text,row,col) in buttons:
+    if text == '=':
+        Button(root, text=text, width=5, height=2,
+               command=calculate).grid(row=row, column=col)
+    else:
+        Button(root, text=text, width=5, height=2,
+               command=lambda t=text: click(t)).grid(row=row, column=col)
+
+Button(root, text="C", width=22, height=2,
+       command=clear).grid(row=5, column=0, columnspan=4)
+
+root.mainloop()
